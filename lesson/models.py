e6b4fe130f05886820e2724f958c4b88f5362880
KaterinaSelevich <katushaaa89@mail.ru>
2022-03-24 12:54:19 +0300

    Itaration_Add cover

--- lesson/models.py
+++ lesson/models.py
@@ -6,31 +6,33 @@ from django.urls import reverse
 
 # Create your models here.
 
-class Material(models.Model):
+class Team(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=255, unique_for_date='publish')
 
     body = models.TextField()
 
+    cover = models.ImageField(upload_to='images/')
+
     created  = models.DateField(auto_now_add=True)
     updeted  = models.DateField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_materials')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_teams')
 
     publish = models.DateTimeField(default=timezone.now())
 
-    MATERIAL_TYPE = [
-        ('theory', 'theoretical material'),
-        ('practice', 'Practical Task'),
+    TEAM_TYPE = [
+        ('группа A', 'Группа А в НБЛ'),
+        ('группа B', 'Группа В в НБЛ'),
     ]
 
-    material_type = models.CharField(max_length=25, choices=MATERIAL_TYPE, default='theory')
+    team_type = models.CharField(max_length=25, choices=TEAM_TYPE, default='руппа А')
 
     # def __str__(self):
     #     return self.title
 
     def get_absolute_url(self):
-        return reverse('lesson:detailed_material',
+        return reverse('lesson:detailed_team',
                        args=[self.publish.year,
                              self.publish.month,
                              self.publish.day,
@@ -38,7 +40,7 @@ class Material(models.Model):
 
 
 class Comment(models.Model):
-    material = models.ForeignKey(Material,
+    team = models.ForeignKey(Team,
                                  on_delete=models.CASCADE,
                                  related_name='comments')
     name = models.CharField(max_length=250)
